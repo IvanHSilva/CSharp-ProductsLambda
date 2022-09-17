f5a8eee5553ce51502fb119017ff731e31f12cbf
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-17 10:55:45 -0300

    Class created

---
+++ ProductsLambda/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace ProductsLambda.Entities {
+    public class Product {
+        // Attributes
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // Constructor
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        // Methods
+        public override string ToString() {
+            return $"{Name}, ${Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- ProductsLambda/Program.cs
+++ ProductsLambda/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace ProductsLambda {
     internal class Program {
         static void Main(string[] args) {
+
         }
     }
 }
